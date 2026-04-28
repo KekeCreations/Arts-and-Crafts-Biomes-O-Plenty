443f860c75771799f972db5f8fdc7eed31a87608
KekeCreations <89083260+KekeCreations@users.noreply.github.com>
2026-04-28 14:49:55 +0100

    Compat Flower Pots Datagen Models

--- fabric/src/main/java/com/kekecreations/arts_and_crafts_biomes_o_plenty/datagen/client/ACBOPModelProvider.java
+++ fabric/src/main/java/com/kekecreations/arts_and_crafts_biomes_o_plenty/datagen/client/ACBOPModelProvider.java
@@ -25,8 +25,139 @@ public class ACBOPModelProvider extends FabricModelProvider {
         for (DyeColor colour : DyeColor.values()) {
             this.createFlowerPot("biomesoplenty", ACBOPBlocks.getDyedOriginFlowerPot(colour), "origin_sapling", colour, generator);
             this.createFlowerPot("biomesoplenty", ACBOPBlocks.getDyedFloweringOakSapling(colour), "flowering_oak_sapling", colour, generator);
-        }
 
+            this.createFlowerPot("biomesoplenty",
+                    ACBOPBlocks.getDyedCypressFlowerPot(colour),
+                    "cypress_sapling", colour, generator);
+
+            this.createFlowerPot("biomesoplenty",
+                    ACBOPBlocks.getDyedSnowblossomFlowerPot(colour),
+                    "snowblossom_sapling", colour, generator);
+
+            this.createFlowerPot("biomesoplenty",
+                    ACBOPBlocks.getDyedRainbowBirchFlowerPot(colour),
+                    "rainbow_birch_sapling", colour, generator);
+
+            this.createFlowerPot("biomesoplenty",
+                    ACBOPBlocks.getDyedFirFlowerPot(colour),
+                    "fir_sapling", colour, generator);
+
+            this.createFlowerPot("biomesoplenty",
+                    ACBOPBlocks.getDyedPineFlowerPot(colour),
+                    "pine_sapling", colour, generator);
+
+            this.createFlowerPot("biomesoplenty",
+                    ACBOPBlocks.getDyedRedMapleFlowerPot(colour),
+                    "red_maple_sapling", colour, generator);
+
+            this.createFlowerPot("biomesoplenty",
+                    ACBOPBlocks.getDyedOrangeMapleFlowerPot(colour),
+                    "orange_maple_sapling", colour, generator);
+
+            this.createFlowerPot("biomesoplenty",
+                    ACBOPBlocks.getDyedYellowMapleFlowerPot(colour),
+                    "yellow_maple_sapling", colour, generator);
+
+            this.createFlowerPot("biomesoplenty",
+                    ACBOPBlocks.getDyedRedwoodFlowerPot(colour),
+                    "redwood_sapling", colour, generator);
+
+            this.createFlowerPot("biomesoplenty",
+                    ACBOPBlocks.getDyedMahoganyFlowerPot(colour),
+                    "mahogany_sapling", colour, generator);
+
+            this.createFlowerPot("biomesoplenty",
+                    ACBOPBlocks.getDyedJacarandaFlowerPot(colour),
+                    "jacaranda_sapling", colour, generator);
+
+            this.createFlowerPot("biomesoplenty",
+                    ACBOPBlocks.getDyedPalmFlowerPot(colour),
+                    "palm_sapling", colour, generator);
+
+            this.createFlowerPot("biomesoplenty",
+                    ACBOPBlocks.getDyedWillowFlowerPot(colour),
+                    "willow_sapling", colour, generator);
+
+            this.createFlowerPot("biomesoplenty",
+                    ACBOPBlocks.getDyedDeadFlowerPot(colour),
+                    "dead_sapling", colour, generator);
+
+            this.createFlowerPot("biomesoplenty",
+                    ACBOPBlocks.getDyedMagicFlowerPot(colour),
+                    "magic_sapling", colour, generator);
+
+            this.createFlowerPot("biomesoplenty",
+                    ACBOPBlocks.getDyedUmbranFlowerPot(colour),
+                    "umbran_sapling", colour, generator);
+
+            this.createFlowerPot("biomesoplenty",
+                    ACBOPBlocks.getDyedHellbarkFlowerPot(colour),
+                    "hellbark_sapling", colour, generator);
+
+            this.createFlowerPot("biomesoplenty",
+                    ACBOPBlocks.getDyedEmpyrealFlowerPot(colour),
+                    "empyreal_sapling", colour, generator);
+
+            this.createFlowerPot("biomesoplenty",
+                    ACBOPBlocks.getDyedRoseFlowerPot(colour),
+                    "rose", colour, generator);
+
+            this.createFlowerPot("biomesoplenty",
+                    ACBOPBlocks.getDyedVioletFlowerPot(colour),
+                    "violet", colour, generator);
+
+            this.createFlowerPot("biomesoplenty",
+                    ACBOPBlocks.getDyedLavenderFlowerPot(colour),
+                    "lavender", colour, generator);
+
+            this.createFlowerPot("biomesoplenty",
+                    ACBOPBlocks.getDyedWhiteLavenderFlowerPot(colour),
+                    "white_lavender", colour, generator);
+
+            this.createFlowerPot("biomesoplenty",
+                    ACBOPBlocks.getDyedOrangeCosmosFlowerPot(colour),
+                    "orange_cosmos", colour, generator);
+
+            this.createFlowerPot("biomesoplenty",
+                    ACBOPBlocks.getDyedPinkDaffodilFlowerPot(colour),
+                    "pink_daffodil", colour, generator);
+
+            this.createFlowerPot("biomesoplenty",
+                    ACBOPBlocks.getDyedPinkHibiscusFlowerPot(colour),
+                    "pink_hibiscus", colour, generator);
+
+            this.createFlowerPot("biomesoplenty",
+                    ACBOPBlocks.getDyedGlowflowerFlowerPot(colour),
+                    "glowflower", colour, generator);
+
+            this.createFlowerPot("biomesoplenty",
+                    ACBOPBlocks.getDyedWiltedLilyFlowerPot(colour),
+                    "wilted_lily", colour, generator);
+
+            this.createFlowerPot("biomesoplenty",
+                    ACBOPBlocks.getDyedBurningBlossomFlowerPot(colour),
+                    "burning_blossom", colour, generator);
+
+            this.createFlowerPot("biomesoplenty",
+                    ACBOPBlocks.getDyedEndbloomFlowerPot(colour),
+                    "endbloom", colour, generator);
+
+            this.createFlowerPot("biomesoplenty",
+                    ACBOPBlocks.getDyedSproutFlowerPot(colour),
+                    "sprout", colour, generator);
+
+            this.createFlowerPot("biomesoplenty",
+                    ACBOPBlocks.getDyedTinyCactusFlowerPot(colour),
+                    "tiny_cactus", colour, generator);
+
+            this.createFlowerPot("biomesoplenty",
+                    ACBOPBlocks.getDyedToadstoolFlowerPot(colour),
+                    "toadstool", colour, generator);
+
+            this.createFlowerPot("biomesoplenty",
+                    ACBOPBlocks.getDyedGlowshroomFlowerPot(colour),
+                    "glowshroom", colour, generator);
+        }
     }
 
     @Override
